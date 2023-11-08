49d89a882777cac49fb46855e1bc4b1645d1cccc
dittoRW <147579048+dittoRW@users.noreply.github.com>
2023-11-08 08:49:04 +0700

    Update Class Point

--- CleanCode/src/cleancode/Point.java
+++ CleanCode/src/cleancode/Point.java
@@ -1,5 +1,9 @@
 package cleancode;
-public class Point {
-    public double x;
-    public double y;
+public interface Point {
+    double getX();
+    double getY();
+    void setCartesian(double x, double y);
+    double getR();
+    double getTheta();
+    void setPolar(double r, double theta);
 }
