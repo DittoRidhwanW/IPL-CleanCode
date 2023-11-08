0da02144af43729125ebf0673b899f7a2bde450c
dittoRW <147579048+dittoRW@users.noreply.github.com>
2023-11-08 08:59:16 +0700

    Legal Comment

---
+++ CleanCode/src/cleancode/GoodComment.java
@@ -0,0 +1,6 @@
+package cleancode;
+public class GoodComment {
+   // Copyright (C) 2011 by Osoco. All rights reserved.
+    // Released under the terms of the GNU General Public License
+    // version 2 or later. 
+}
