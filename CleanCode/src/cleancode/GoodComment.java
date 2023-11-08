598f393910bb1780480f17be2fead1080badc158
dittoRW <147579048+dittoRW@users.noreply.github.com>
2023-11-08 09:00:25 +0700

    Informative Comment

--- CleanCode/src/cleancode/GoodComment.java
+++ CleanCode/src/cleancode/GoodComment.java
@@ -1,6 +1,9 @@
 package cleancode;
 public class GoodComment {
-   // Copyright (C) 2011 by Osoco. All rights reserved.
-    // Released under the terms of the GNU General Public License
-    // version 2 or later. 
+    // Returns an instance of the Responder being tested.
+    protected abstract Responder responderInstance();
+    // renaming the function: responderBeingTested
+    // format matched kk:mm:ss EEE, MMM dd, yyyy
+    Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*,
+    \\d*"); 
 }
